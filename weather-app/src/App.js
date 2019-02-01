0a5efdfffc1784fc920664b8878e4eedeba448bb
Divesh Pandey <divesh.pandey.cse15@iitbhu.ac.in>
2019-02-01 23:38:55 +0530

    Updates frontend part

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -65,16 +65,29 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Titles />
-        <Form loadWeather={this.getWeather}/>
-        <Weather 
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          humidity={this.state.humidity}
-          desc={this.state.desc}
-          error={this.state.error}/>
-      </div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-2"></div>
+                <div className="col-xs-8">
+                    <Titles />
+                    <Form loadWeather={this.getWeather}/>
+                    <Weather 
+                      temperature={this.state.temperature}
+                      city={this.state.city}
+                      country={this.state.country}
+                      humidity={this.state.humidity}
+                      desc={this.state.desc}
+                      error={this.state.error}/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
     )
   }
 }
