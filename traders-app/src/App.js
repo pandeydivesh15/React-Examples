70a634f58983551b8592aee7ea113247e48ffaf5
Divesh Pandey <divesh.pandey.cse15@iitbhu.ac.in>
2019-06-20 15:34:52 +0530

    Starts new project: Trader's App

---
+++ traders-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <h2>React Traders Book</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
